0bbe1f6e3e9b5bde8c85db827dd45bc01d500f62
NighttfallGT <104257061+NighttfallGT@users.noreply.github.com>
2022-07-25 10:53:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+
+namespace TokenLogin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "TokenLogin | by deep1dark";
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write("Token: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            string token = Console.ReadLine();
+            Console.Clear();
+            ChromeDriver chrome = new ChromeDriver();
+            chrome.Navigate().GoToUrl("https://discord.com/login");
+            try
+            {
+                chrome.ExecuteScript("function login(token) {setInterval(() => {document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage.token = `\"${token}\"`}, 50);setTimeout(() => {location.reload();}, 0);} login(\"" + token + "\")");
+                Console.Clear();
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
